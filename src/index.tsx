001e2b948c717f28430761e3ba8f0abfce24e424
Daan van den Berg <mail@daanvandenberg.com>
2022-05-16 16:54:37 +0200

    Create global context provider for channel and user

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ApolloProvider } from "@apollo/client";
 import client from './graphql/client';
+import StateProvider from './globalContext';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <ApolloProvider client={client}>
-            <App/>
+            <StateProvider>
+                <App/>
+            </StateProvider>
         </ApolloProvider>
     </React.StrictMode>
 );
