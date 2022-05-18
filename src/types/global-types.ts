060600c64684b3563f678e10bfd2abaa6373af68
Daan van den Berg <mail@daanvandenberg.com>
2022-05-18 16:37:57 +0200

    Add apollo codegen support to improve typing

---
+++ src/types/global-types.ts
@@ -0,0 +1,11 @@
+export enum ChannelId {
+  General = "General",
+  LGTM = "LGTM",
+  Technology = "Technology",
+}
+
+export enum UserId {
+  Joyse = "Joyse",
+  Russell = "Russell",
+  Sam = "Sam",
+}
